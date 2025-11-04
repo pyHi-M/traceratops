24c08de375e5b0a0ec3494d1a017b567a59b3543
XDevos <92782954+XDevos@users.noreply.github.com>
2025-11-04 10:22:43 +0100

    Feat/format spot table (#62)
    * feat: Reformat localizations table #281
    
    * fix: duplicate .4dn extension name

--- traceratops/core/localization_table.py
+++ traceratops/core/localization_table.py
@@ -9,20 +9,151 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from astropy.table import Table, vstack
-
-
-def read_table_from_ecsv(path):
-    """Read an astropy Table saved as an ``ecsv`` file."""
-    # read ecsv file
-    table = Table.read(path, format="ascii.ecsv")
-
-    return table
 
 
 class LocalizationTable:
     def __init__(self):
         self.a = 1
+        self.original_format = "ecsv"  # Default format
+        self.fof_ct_version = "v1.0"
+        self.table_namespace = "4dn_FOF-CT_demultiplexing"
+        self.xyz_unit = "pixel"
+        self.lab_name = "NollmannLab"
+        self.experimenter_name = ""
+        self.experimenter_contact = ""
+        self.description = ""
+        self.software_title = ""
+        self.software_type = ""
+        self.software_authors = ""
+        self.software_description = ""
+        self.software_repository = ""
+        self.software_citation = ""
+        self.additional_tables = "4dn_FOF-CT_core"
+        self.columns = [
+            "Loc_ID",
+            "ROI #",
+            "CellID #",
+            "Barcode #",
+            "Spot_ID",
+            "Z",
+            "X",
+            "Y",
+            "sharpness",
+            "roundness1",
+            "roundness2",
+            "npix",
+            "sky",
+            "peak",
+            "flux",
+            "mag",
+        ]
+
+    def _read_metadata_from_4dn(self, file):
+        """
+        Reads metadata fields from a .4dn file and stores them as class attributes.
+        """
+        with open(file, "r") as f:
+            for line in f:
+                if line.startswith("##FOF-CT_Version="):
+                    self.fof_ct_version = line.split("=")[1].strip()
+                elif line.startswith("#Table_Namespace="):
+                    self.table_namespace = line.split("=")[1].strip()
+                elif line.startswith("#XYZ_Unit="):
+                    self.xyz_unit = line.split("=")[1].strip()
+                elif line.startswith("#Lab_Name:"):
+                    self.lab_name = line.split(": ")[1].strip()
+                elif line.startswith("#Experimenter_Name:"):
+                    self.experimenter_name = line.split(": ")[1].strip()
+                elif line.startswith("#Experimenter_Contact:"):
+                    self.experimenter_contact = line.split(": ")[1].strip()
+                elif line.startswith("#Description:"):
+                    self.description = line.split(": ")[1].strip()
+                elif line.startswith("#Software_Type:"):
+                    self.software_type = line.split(": ")[1].strip()
+
+                elif line.startswith("#Software_Title:"):
+                    self.software_title = line.split(": ")[1].strip()
+                elif line.startswith("#Software_Authors:"):
+                    self.software_authors = line.split(": ")[1].strip()
+                elif line.startswith("#Software_Description:"):
+                    self.software_description = line.split(": ")[1].strip()
+                elif line.startswith("#Software_Repository:"):
+                    self.software_repository = line.split(": ")[1].strip()
+                elif line.startswith("#Software_PreferredCitationID:"):
+                    self.software_citation = line.split(": ")[1].strip()
+                elif line.startswith("#Additional_Tables:"):
+                    self.additional_tables = line.split(": ")[1].strip()
+                elif line.startswith("##columns="):
+                    self.columns = line.split("=")[1].split("(")[1].split(")")[0]
+                    self.columns = self.columns.split(", ")
+                    # self.columns = self._read_column_names_from_4dn(file)
+                    print(f"> Columns read: {self.columns}")
+
+    def _convert_4dn_to_astropy(self, fofct_file):
+        """
+        Converts a .4dn file to an Astropy table with appropriate formatting.
+        Also saves a BED file mapping genomic coordinates to barcode numbers.
+        """
+        column_names = self.columns
+        csv_data = pd.read_csv(fofct_file, comment="#", header=None, names=column_names)
+
+        # Rename columns for Astropy compatibility
+        csv_data.rename(
+            columns={
+                "Loc_ID": "Buid",
+                "Spot_ID": "id",
+                "X": "xcentroid",
+                "Y": "ycentroid",
+                "Z": "zcentroid",
+            },
+            inplace=True,
+        )
+
+        return Table.from_pandas(csv_data)
+
+    def _convert_astropy_to_4dn(self, table, output_file):
+        """
+        Converts an Astropy table back to .4dn format with appropriate headers.
+        """
+        output_file = output_file.strip(".ecsv").strip(".4dn") + ".4dn"
+
+        csv_data = table.to_pandas()
+        csv_data.rename(
+            columns={
+                "Buid": "Loc_ID",
+                "id": "Spot_ID",
+                "xcentroid": "X",
+                "ycentroid": "Y",
+                "zcentroid": "Z",
+            },
+            inplace=True,
+        )
+
+        # parses column list for header
+        column_list = ", ".join(self.columns)
+
+        header = f"""##FOF-CT_Version={self.fof_ct_version}
+##Table_Namespace={self.table_namespace}
+##XYZ_Unit={self.xyz_unit}
+#Lab_Name: {self.lab_name}
+#Experimenter_Name: {self.experimenter_name}
+#Experimenter_Contact: {self.experimenter_contact}
+#Description: {self.description}
+#Software_Title: {self.software_title}
+#Software_Type: {self.software_type}
+#Software_Authors: {self.software_authors}
+#Software_Description: {self.software_description}
+#Software_Repository: {self.software_repository}
+#Software_PreferredCitationID: {self.software_citation}
+#Additional_Tables: {self.additional_tables}
+##Columns=({column_list})
+"""
+        with open(output_file, "w") as f:
+            f.write(header)
+            csv_data.to_csv(f, index=False, header=False)
+        print(f"Saved 4dn spot table with headers: {output_file}")
 
     def load(self, file):
         """
@@ -40,25 +171,46 @@ class LocalizationTable:
             lis of unique barcodes read from barcode_map
 
         """
-        if os.path.exists(file):
-            barcode_map = read_table_from_ecsv(file)
 
-            print(f"$ Successfully loaded barcode localizations file: {file}")
-
-            unique_barcodes = np.unique(barcode_map["Barcode #"].data)
-            number_unique_barcodes = unique_barcodes.shape[0]
-
-            print(
-                f"$ Number of barcodes read from barcode_map: {number_unique_barcodes}"
-            )
-            print(f"$ Unique Barcodes detected: {unique_barcodes}")
-        else:
-            print(f"\n\n# ERROR: could not find coordinates file: {file}")
+        if not os.path.exists(file):
+            print(f"# ERROR: could not find coordinates file: {file}")
             sys.exit()
+
+        file_ext = os.path.splitext(file)[1].lower()
+        if file_ext == ".ecsv":
+            print("$ Importing table from pyHiM format")
+            barcode_map = read_table_from_ecsv(file)
+            self.original_format = "ecsv"
+        elif file_ext == ".4dn":
+            print("$ Importing table from fof-ct format")
+            self._read_metadata_from_4dn(file)
+            barcode_map = self._convert_4dn_to_astropy(file)
+            self.data = barcode_map
+            self.original_format = "4dn"
+        else:
+            raise ValueError("Unsupported file format. Use .ecsv or .4dn")
+
+        print(f"$ Successfully loaded barcode localizations file: {file}")
+
+        unique_barcodes = np.unique(barcode_map["Barcode #"].data)
+        number_unique_barcodes = unique_barcodes.shape[0]
+
+        print(f"$ Number of barcodes read from barcode_map: {number_unique_barcodes}")
+        print(f"$ Unique Barcodes detected: {unique_barcodes}")
 
         return barcode_map, unique_barcodes
 
-    def save(self, file_name, barcode_map, comments=""):
+    def remove_empty_comments(self):
+        if len(self.data.meta["comments"]):
+            self.data.meta["comments"] = [
+                com for com in self.data.meta["comments"] if com
+            ]
+
+    def remove_duplicate_comments(self):
+        if len(self.data.meta["comments"]):
+            self.data.meta["comments"] = list(dict.fromkeys(self.data.meta["comments"]))
+
+    def save(self, file_name, barcode_map, comments="", format="4dn"):
         """
         Saves output table
 
@@ -80,18 +232,22 @@ class LocalizationTable:
         None.
 
         """
-        print(f"$ Saving output table as {file_name} ...")
+        if format == "4dn":
+            self._convert_astropy_to_4dn(barcode_map, file_name)
+        else:
+            print(f"$ Saving output table as {file_name} ...")
+            self.remove_empty_comments()
+            self.remove_duplicate_comments()
+            try:
+                barcode_map.meta["comments"].append(comments)
+            except KeyError:
+                barcode_map.meta["comments"] = [comments]
 
-        try:
-            barcode_map.meta["comments"].append(comments)
-        except KeyError:
-            barcode_map.meta["comments"] = [comments]
-
-        barcode_map.write(
-            file_name,
-            format="ascii.ecsv",
-            overwrite=True,
-        )
+            barcode_map.write(
+                file_name,
+                format="ascii.ecsv",
+                overwrite=True,
+            )
 
     def append(self, table1, table2):
         """
@@ -343,3 +499,53 @@ def plots_localization_projection(coord1, coord2, axis, colors, title="" * 3):
     """
     axis.scatter(coord1, coord2, s=5, c=colors, alpha=0.9, cmap="hsv")  # nipy_spectral
     axis.set_title(title)
+
+
+def read_table_from_ecsv(path):
+    """Read an astropy Table saved as an ``ecsv`` file."""
+    # read ecsv file
+    table = Table.read(path, format="ascii.ecsv")
+
+    return table
+
+
+def create_output_table():
+    output = Table(
+        names=(
+            "Buid",
+            "ROI #",
+            "CellID #",
+            "Barcode #",
+            "id",
+            "zcentroid",
+            "xcentroid",
+            "ycentroid",
+            "sharpness",
+            "roundness1",
+            "roundness2",
+            "npix",
+            "sky",
+            "peak",
+            "flux",
+            "mag",
+        ),
+        dtype=(
+            "S2",
+            "int",
+            "int",
+            "int",
+            "int",
+            "f4",
+            "f4",
+            "f4",
+            "f4",
+            "f4",
+            "f4",
+            "int",
+            "f4",
+            "f4",
+            "f4",
+            "f4",
+        ),
+    )
+    return output
