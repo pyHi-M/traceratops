9c7b71bbf3fd0a94c08952276dee8e398cee2cf5
NollmannLab <marcnol@gmail.com>
2026-07-07 12:26:03 +0200

    Update plot_him_matrix tests for nan filenames

--- traceratops/plot_him_matrix.py
+++ traceratops/plot_him_matrix.py
@@ -62,10 +62,8 @@ Outputs:
     )
     parser_advanced.add_argument(
         "--output_format",
-        "--plot_format",
         choices=["png", "svg", "pdf"],
         default="png",
-        dest="output_format",
         help="Output image format. Default = png.",
     )
     parser_advanced.add_argument(
